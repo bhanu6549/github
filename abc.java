780b2b0d44ec7ac52e3689f1f21db1dc41fb16d9
bhanu6549 <M1057752@mindtree.com>
2020-03-19 23:07:20 +0530

    committed by bhanu6549

--- abc.java
+++ abc.java
@@ -2,7 +2,9 @@ public class abc
 {
 public static void main(String args[])
 {
-System.out.println("Hii this is ");
-Sysout .out.println;
+  for(int i=0;i<10;i++)
+  {
+	  System.out.println(i)
+  }
 }
 }
